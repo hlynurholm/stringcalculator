f5d04a9034dded6f405b58453b78201d548451c5
Hlynur Holm Hauksson <hlynurholm97@gmail.com>
2018-10-13 19:54:25 +0000

    var í ruglinu með git, núna komið í lag. Add multiple numbers together ásamt að virka með new line og comma

--- calculator.js
+++ calculator.js
@@ -1,5 +1,17 @@
-function  add(number){
-	return 0;
+function add(number){
+	if(number == ""){
+		return 0;
+	}
+
+	if(number.includes(",") || number.includes("\\n")){
+		var numArray = number.split(/[\n,]/);
+		var sum = 0;
+		for(var i = 0; i < numArray.length; i++){
+			sum += parseInt(numArray[i]);
+		}
+		return sum;
+	}
+
 }
 
-module.export;
+module.exports = add;
--- calculator.test.js
+++ calculator.test.js
@@ -2,4 +2,16 @@ const add = require("./calculator"); //import calculator
 
 it("should return zero on an empty string", () => {
 	expect(add("")).toBe(0);
-)};
+});
+
+it("should return sum of multiple numbers", () => {
+	expect(add("1,2,3,4")).toBe(10);
+});
+
+it("should return sum of multiple numbers (also negative numbers", () => {
+	expect(add("1,2,-3,4")).toBe(4);
+});
+
+it("should return sum of multiple numbers divided by commas and new line", () => {
+	expect(add("1\n2,3")).toBe(6);
+});
